911147fa959f923e1f04852cc176570ba2be4c5d
ManjuTech-3249 <55014369+ManjuTech-3249@users.noreply.github.com>
2020-03-29 22:08:27 +0530

    Add files via upload

--- HivetoJson/src/main/java/com/npntraining/HivetoJson/HiveController.java
+++ HivetoJson/src/main/java/com/npntraining/HivetoJson/HiveController.java
@@ -1,44 +1,28 @@
 package com.npntraining.HivetoJson;
 
-import java.sql.*;
-import java.sql.DriverManager;
-import java.util.ArrayList;
-import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.npntraining.scala_test.MariaDBExample;
+
 
 @RestController
 @EnableAutoConfiguration
 public class HiveController {
 	
-	 @SuppressWarnings("finally")
-	@RequestMapping("/")
-	 public List<Employee> getEmployee(){
+	 
+	@RequestMapping(value = "/employee/{country_code}", method = RequestMethod.GET)
+	 public String getEmployee(@PathVariable("country_code") String country_code){
 		 
-		 List<Employee> list = new ArrayList<Employee>();	
-		 
-		try {
-			Class.forName("org.apache.hive.jdbc.HiveDriver");
-			Connection connection = DriverManager.getConnection("jdbc:hive2://localhost:10000/emp_m", "npntraining",
-					"npntraining")  ;
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery("select * from customer");
-			
-			while(rs.next()) {
-				Employee e = new Employee();
-				e.ename = rs.getString("name");
-				e.age = rs.getInt("age");
-				list.add(e);
-			}
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-		finally{
-			return list;
-		}
+		 JSONObject obj = new JSONObject();
+		 obj = MariaDBExample.getJSONData(country_code);
+		 System.out.println(obj);
+		return obj.toString();
 		 
 	 }
 }
